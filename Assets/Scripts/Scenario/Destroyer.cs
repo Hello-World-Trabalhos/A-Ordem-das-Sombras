d35973db524ee8ca3bd60feb3f17b918e0e337f4
Michael Toninger <michaeltp.2012@hotmail.com>
2022-05-04 17:27:54 -0300

    Adicionado spawn de closed room quando ha aberturas para o limbo

--- Assets/Scripts/Scenario/Destroyer.cs
+++ Assets/Scripts/Scenario/Destroyer.cs
@@ -6,21 +6,15 @@ public class Destroyer : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
-        // TODO: Depois alterar o nome desse SpawnPointDestroyer, pois ele poderá
-        // ser usado para destruir os quartos fechados também
-        // outro ponto é ter cuidado para não destruir o player que entrar na área
-        // desse objeto
-        // TODO: fazer as tratativas comparando e vendo se os objetos são esses mesmos
+        
         switch (other.tag)
         {
             case "RoomSpawnPoint":
-                Debug.Log("Destruindo RoomSpawnPoint que colidiu no destroyer");
                 Destroy(other.gameObject);
                 break;
 
-            case "ClosedRoom":
-                // Destruir o closed room, evitando que ele fique dentro de um room sem querer
-                Debug.Log("Destruindo ClosedRoom que colidiu no destroyer");
+            case "Wall":
+                Destroy(other.transform.parent.gameObject);
                 break;
         }
     }
