11279a4a9bfc07c8f22a8ee17d6e52fb1b8a52ef
Michael Toninger <michaeltp.2012@hotmail.com>
2022-08-18 17:45:49 -0300

    Adicionado coleta de templates interiores no ScenarioGenerationManager

--- Assets/Scripts/Scenario/ScenarioGenerationManager.cs
+++ Assets/Scripts/Scenario/ScenarioGenerationManager.cs
@@ -7,12 +7,14 @@ public class ScenarioGenerationManager : MonoBehaviour
 
     private RoomsStorage roomsStorage;
     private InteriorRoomSpawner interiorRoomSpawner;
+    private InteriorRoomStorage interiorRoomStorage;
 
     void Start()
     {
         roomsStorage = GameObject.Find("RoomsStorage").GetComponent<RoomsStorage>();
         interiorRoomSpawner = GameObject.Find("InteriorRoomSpawner").GetComponent<InteriorRoomSpawner>();
-
+        interiorRoomStorage = GameObject.Find("InteriorRoomStorage").GetComponent<InteriorRoomStorage>();
+        
         Invoke("PrepareScenario", ScenarioConstants.TIME_TO_START_SCENARIO_PREPARATION);
     }
 
@@ -21,5 +23,7 @@ public class ScenarioGenerationManager : MonoBehaviour
         // Aqui conterá todas as chamadas necessárias para gerenciar a criação do cenário
         roomsStorage.CollectAllSpawnedRooms();
         interiorRoomSpawner.SpawnInteriorRoomsTemplates();
+        interiorRoomStorage.CollectAllSpawnedInteriorTemplates();
+
     }
 }
