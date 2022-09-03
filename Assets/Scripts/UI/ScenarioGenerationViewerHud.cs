4218cfb09e3c950fd1f2f1b076bdc6fa4f457cbc
Michael Toninger <michaeltp.2012@hotmail.com>
2022-09-03 18:17:53 -0300

    Adicionado botão para gerar novo cenario procedural

--- Assets/Scripts/UI/ScenarioGenerationViewerHud.cs
+++ Assets/Scripts/UI/ScenarioGenerationViewerHud.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ScenarioGenerationViewerHud : MonoBehaviour
 {
     private Button pauseButton;
+    private Button scenarioGeneratorButton;
     private GameObject pausePanel;
     private TouchActions touchActions;
 
@@ -14,12 +15,14 @@ public class ScenarioGenerationViewerHud : MonoBehaviour
         pauseButton = gameObject.transform.Find("PauseButton").GetComponent<Button>();
         pausePanel = gameObject.transform.Find("PausePanel").gameObject;
         touchActions = GameObject.Find("TouchActions").GetComponent<TouchActions>();
+        scenarioGeneratorButton = GameObject.Find("ScenarioGeneratorButton").GetComponent<Button>();
     }
 
     public void PauseGame()
     {
         // Lembrar de adicionar pausa de tempo aqui
         pauseButton.gameObject.SetActive(false);
+        scenarioGeneratorButton.gameObject.SetActive(false);
         touchActions.gameObject.SetActive(false);
         pausePanel.SetActive(true);
     }
@@ -27,8 +30,14 @@ public class ScenarioGenerationViewerHud : MonoBehaviour
     public void ResumeGame()
     {
         pauseButton.gameObject.SetActive(true);
+        scenarioGeneratorButton.gameObject.SetActive(true);
         touchActions.gameObject.SetActive(true);
         pausePanel.SetActive(false);
+    }
+
+    public void GenerateNewScenario()
+    {
+        SceneLoader.ReloadCurrentScene();
     }
 
     public void LoadMainMenu()
