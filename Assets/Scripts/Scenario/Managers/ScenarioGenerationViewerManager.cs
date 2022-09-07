5c564cd307165f8005abe6c591d9e8b12f5e8a54
Michael Toninger <michaeltp.2012@hotmail.com>
2022-09-07 09:26:29 -0300

    adicionado ScenarioGenerationViewerManager

---
+++ Assets/Scripts/Scenario/Managers/ScenarioGenerationViewerManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenarioGenerationViewerManager : MonoBehaviour
+{
+    private RoomsStorage roomsStorage;
+    private InteriorRoomSpawner interiorRoomSpawner;
+    private InteriorRoomStorage interiorRoomStorage;
+    private ObstacleSpawner obstacleSpawner;
+    private EnemySpawner enemySpawner;
+    private BossSpawner bossSpawner;
+    private PlayerSpawner playerSpawner;
+    private RoomSpawnWait roomSpawnWait;
+
+    private TimeUtils timeUtils = new TimeUtils();
+
+    void Start()
+    {
+        Debug.Log("Esse é o novo, que deve substituir a outra prefab");
+        roomSpawnWait = GameObject.Find("RoomSpawnWait").GetComponent<RoomSpawnWait>();
+        roomSpawnWait.ResetTimeToWaitRoomsSpawn();
+        StartCoroutine(WaitAllRoomsBeSpawned());
+
+        roomsStorage = GameObject.Find("RoomsStorage").GetComponent<RoomsStorage>();
+        interiorRoomSpawner = GameObject.Find("InteriorRoomSpawner").GetComponent<InteriorRoomSpawner>();
+        interiorRoomStorage = GameObject.Find("InteriorRoomStorage").GetComponent<InteriorRoomStorage>();
+        obstacleSpawner = GameObject.Find("ObstacleSpawner").GetComponent<ObstacleSpawner>();
+        enemySpawner = GameObject.Find("EnemySpawner").GetComponent<EnemySpawner>();
+        bossSpawner = GameObject.Find("BossSpawner").GetComponent<BossSpawner>();
+        playerSpawner = GameObject.Find("PlayerSpawner").GetComponent<PlayerSpawner>();
+    }
+
+    private IEnumerator WaitAllRoomsBeSpawned()
+    {
+        while (!roomSpawnWait.IsPossibleToStartScenarioPreparation())
+        {
+            yield return new WaitForSeconds(roomSpawnWait.GetTimeToWaitRoomsSpawn());
+        }
+
+        PrepareScenario();
+        StopCoroutine(WaitAllRoomsBeSpawned());
+    }
+
+    private void PrepareScenario()
+    {
+        interiorRoomSpawner.SpawnInteriorRoomsTemplates();
+        obstacleSpawner.SpawnObstacles();
+        enemySpawner.SpawnEnemies();
+        bossSpawner.SpawnBoss();
+        playerSpawner.SpawnPlayer();
+    }
+}
