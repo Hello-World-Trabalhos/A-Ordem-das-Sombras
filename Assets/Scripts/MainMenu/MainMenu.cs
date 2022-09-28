e89a610b03baa6e6a2bc5d2c83421fda7890fc67
Michael Toninger <michaeltp.2012@hotmail.com>
2022-09-27 21:01:08 -0300

    Adicionado funcionalidades para carregar cenas

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -2,6 +2,28 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+
+    private GameObject mainMenu;
+    private GameObject scenarioGenerationPanel;
+
+    void Start()
+    {
+        mainMenu = GameObject.Find("MainMenu").gameObject;
+        scenarioGenerationPanel = GameObject.Find("Canvas").transform.Find("ScenarioGenerationPanel").gameObject;
+    }
+
+    public void OpenScenarioGenerationPanel()
+    {
+        mainMenu.SetActive(false);
+        scenarioGenerationPanel.SetActive(true);
+    }
+
+    public void CloseScenarioGenerationPanel()
+    {
+        scenarioGenerationPanel.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
     public void ExitGame()
     {
         Application.Quit();
