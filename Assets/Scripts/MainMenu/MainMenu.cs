52a1b20bb2a2997a3dc3604fafa6f919092eaab8
Michael Toninger <michaeltp.2012@hotmail.com>
2022-09-29 17:30:16 -0300

    Adicionado modal de configuracoes

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
@@ -6,6 +7,7 @@ public class MainMenu : MonoBehaviour
     private GameObject mainMenu;
     private GameObject scenarioGenerationPanel;
     private GameObject aboutPanel;
+    private GameObject settingsPanel;
 
     void Start()
     {
@@ -14,6 +16,7 @@ public class MainMenu : MonoBehaviour
         mainMenu = canvas.transform.Find("MainMenu").gameObject;
         scenarioGenerationPanel = canvas.transform.Find("ScenarioGenerationPanel").gameObject;
         aboutPanel = canvas.transform.Find("AboutPanel").gameObject;
+        settingsPanel = canvas.transform.Find("SettingsPanel").gameObject;
     }
 
     public void ToggleScenarioGenerationPanel()
@@ -26,6 +29,33 @@ public class MainMenu : MonoBehaviour
     {
         mainMenu.SetActive(!mainMenu.activeSelf);
         aboutPanel.SetActive(!aboutPanel.activeSelf);
+    }
+
+    public void ToggleSettingsPanel()
+    {
+        mainMenu.SetActive(!mainMenu.activeSelf);
+        settingsPanel.SetActive(!settingsPanel.activeSelf);
+
+        if (settingsPanel.activeSelf)
+        {
+            SetupConfigs();
+        }
+    }
+
+    public void SaveConfigs()
+    {
+        Toggle musicToggle = settingsPanel.transform.Find("MusicToggle").transform.Find("Toggle").GetComponent<Toggle>();
+        Slider musicVolume = settingsPanel.transform.Find("MusicVolumeSlider").transform.Find("Slider").GetComponent<Slider>();
+        // chamar o AudioConfigSaver (instância)
+        // chamar o método dele para salvar configuracoes
+    }
+
+    private void SetupConfigs()
+    {
+        Toggle musicToggle = settingsPanel.transform.Find("MusicToggle").transform.Find("Toggle").GetComponent<Toggle>();
+        Slider musicVolume = settingsPanel.transform.Find("MusicVolumeSlider").transform.Find("Slider").GetComponent<Slider>();
+        // buscar os valores do AudioConfig
+        // atribuir aos itens referenciados nas variáveis acima
     }
 
     public void ExitGame()
